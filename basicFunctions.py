05762dc945a1466d0d958d8e09a7a5f8d60f040c
Adam von Arnim <vonarnim@usc.edu>
2022-02-20 20:25:12 -0800

    transformations

--- basicFunctions.py
+++ basicFunctions.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('guy.jpeg')
 
@@ -33,5 +34,27 @@ cv.imshow('Resized', resized)
 # Cropping image
 cropped = img[200:300, 200:300]
 cv.imshow('Cropped', cropped)
+
+def translate(img, xShift, yShift):
+    transMat = np.float32([[1, 0, xShift], [0, 1, yShift]])
+    # width & height
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
+    rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+    return cv.warpAffine(img, rotMat, dimensions)
+
+shifted = translate(img, 100, 100)
+cv.imshow('Translated', shifted)
+
+rotated = rotate(img, 45)
+cv.imshow('Rotated', rotated)
+
+# Can also do resizing, flipping, etc. to an image
 
 cv.waitKey(0)
